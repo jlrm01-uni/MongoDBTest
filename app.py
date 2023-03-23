3cc4935c6e979c05d72e5a76b7ec92ef6925243f
Jorge Rodriguez <jorge.rodriguez@guayama.inter.edu>
2023-03-23 14:25:45 -0400

    navigation bar creature descriptions prettify stuff

--- app.py
+++ app.py
@@ -26,5 +26,12 @@ def creature():
     return render_template("creature.html", name=name, creature=creature)
 
 
+@app.route("/abilities")
+def abilities():
+    all_abilities = Ability.objects()
+
+    return render_template("abilities.html", all_abilities=all_abilities)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
